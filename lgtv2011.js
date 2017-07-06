1bab0048f046e9160e4d2db0633965d2de7cad9b
Sebastian Mundt <mail@sebastian-mundt.com>
2017-07-06 13:11:37 +0200

    Update lgtv2011.js

--- lgtv2011.js
+++ lgtv2011.js
@@ -3,16 +3,14 @@ var fs 				= require('fs'); // for storing client key
 var utils 			= require(__dirname + '/lib/utils');
 var adapter 		= utils.adapter('lgtv2011');
 var LGTV            = require('node-lgtv-api');
-var pollTimerChannel       = null;
-var pollTimerOnlineStatus       = null;
-var pollTimerInput       = null;
 
-function RequestPairingKey(ip) {
-	adapter.log.debug('Requesting Pairing Key on TV: ' + adapter.config.ip);
+function RequestPairingKey(ip, port) 
+{
+	adapter.log.info('Requesting Pairing Key on TV: ' + adapter.config.ip);
 	var lgtvobj = new LGTV(adapter.config.ip, adapter.config.port);
-	lgtvobj.displayPairingKey(function (err) {
-		if (err) 
-			adapter.log.debug('ERROR: ' + err);
+	lgtvobj.displayPairingKey(function (err) 
+	{
+		if (err) adapter.log.error('ERROR: ' + err);
     })
 }
 
@@ -24,25 +22,23 @@ adapter.on('stateChange', function (id, state)
 	}
 });
 
-function onMessage (obj) {
-    if (!obj) return;
-
-    function reply(result) {
-        adapter.sendTo (obj.from, obj.command, JSON.stringify(result), obj.callback);
-    }
-
-    switch (obj.command) {
+adapter.on('message', function (obj) 
+{
+	adapter.log.debug('Incoming Adapter message: ' + obj.command);
+    switch (obj.command) 
+	{
         case 'RequestPairingKey_Msg':
             if (!obj.callback) return false;
-			RequestPairingKey(adapter.config.ip);
-            return true;
+			RequestPairingKey(adapter.config.ip, adapter.config.port);
+		return true;
+		
         default:
             adapter.log.warn("Unknown command: " + obj.command);
-            break;
+		break;
     }
-    if (obj.callback) adapter.sendTo (obj.from, obj.command, obj.message, obj.callback);
-    return true;
-}
+});
+
+
 
 adapter.on('ready', main);
 
@@ -50,9 +46,4 @@ function main()
 {
 	adapter.log.info('Ready. Configured TV IP: ' + adapter.config.ip);
     //adapter.subscribeStates('*');
-	//if (parseInt(adapter.config.interval, 10)) {
-//		pollTimerChannel = setInterval(pollChannel, parseInt(adapter.config.interval, 10));
-//		pollTimerOnlineStatus = setInterval(pollOnlineStatus, parseInt(adapter.config.interval, 10));
-//		pollTimerInput = setInterval(pollInput, parseInt(adapter.config.interval, 10));
-//	}
 }
